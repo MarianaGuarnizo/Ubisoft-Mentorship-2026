7f005e079f24bd3d728adace58b89f8167bbb6f4
MarianaGuarnizo <mariana.guarnizo@gmail.com>
2026-02-05 18:23:48 +0100

    Added Crates, Crystals and Healbox SFX and creted Events

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,12 +14,31 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID CONFIRM = 1517537939U;
+        static const AkUniqueID CRATEBREAK = 2377317199U;
+        static const AkUniqueID CRATEFALL = 402727457U;
+        static const AkUniqueID CRYSTAL = 3444057113U;
+        static const AkUniqueID CRYSTALPICKUP = 3943406705U;
+        static const AkUniqueID HEALBOXIDLE = 3222341092U;
+        static const AkUniqueID HEALBOXOPEN = 2128079350U;
         static const AkUniqueID HEARTGAIN = 1893120954U;
         static const AkUniqueID HEARTLOSS = 1695606510U;
         static const AkUniqueID HOVER = 3753593413U;
         static const AkUniqueID RETURN = 3859834159U;
         static const AkUniqueID SELECT = 1432588725U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACETYPE
+        {
+            static const AkUniqueID GROUP = 63790334U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace SURFACETYPE
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
