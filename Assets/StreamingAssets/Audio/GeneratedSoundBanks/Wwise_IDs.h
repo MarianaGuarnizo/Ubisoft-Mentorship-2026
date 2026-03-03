2d313999e25a3712fab926e7357afcf1028f568c
MarianaGuarnizo <mariana.guarnizo@gmail.com>
2026-03-03 17:05:38 +0100

    Added SFX to Wwise for Lvl 1

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,32 +13,96 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BLUEPLANT = 3675248848U;
+        static const AkUniqueID CHOMPATTACK = 897397800U;
+        static const AkUniqueID CHOMPDISAPEAR = 546661981U;
+        static const AkUniqueID CHOMPFTSTPS = 501939944U;
+        static const AkUniqueID CHOMPHIT = 120692987U;
+        static const AkUniqueID CHOMPIDLE = 3236507202U;
+        static const AkUniqueID CHOMPSPOTTED = 2941029371U;
         static const AkUniqueID CONFIRM = 1517537939U;
         static const AkUniqueID CRATEBREAK = 2377317199U;
         static const AkUniqueID CRATEFALL = 402727457U;
         static const AkUniqueID CRYSTAL = 3444057113U;
         static const AkUniqueID CRYSTALPICKUP = 3943406705U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID EMOTELANDING = 1399379220U;
+        static const AkUniqueID FIREFLIES = 1126065684U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GRENADEBOUNCE = 2267468973U;
+        static const AkUniqueID GRENADEEXPLOSION = 2310057666U;
+        static const AkUniqueID GRENADEWHOOSH = 2053280679U;
+        static const AkUniqueID GRENADIERCLOSERANGEATTACK = 590881027U;
+        static const AkUniqueID GRENADIERDEATH = 1511889326U;
+        static const AkUniqueID GRENADIEREXPLOSION = 1236530165U;
+        static const AkUniqueID GRENADIERFTSTPS = 3604079796U;
+        static const AkUniqueID GRENADIERHIT = 3414813087U;
+        static const AkUniqueID GRENADIERMELEEATTACK = 528823072U;
+        static const AkUniqueID GRENADIERRANGEATTACK = 4242898983U;
+        static const AkUniqueID HANGINGMOSS = 2095273585U;
+        static const AkUniqueID HANGINGVINES = 3182403742U;
         static const AkUniqueID HEALBOXIDLE = 3222341092U;
         static const AkUniqueID HEALBOXOPEN = 2128079350U;
         static const AkUniqueID HEARTGAIN = 1893120954U;
         static const AkUniqueID HEARTLOSS = 1695606510U;
         static const AkUniqueID HOVER = 3753593413U;
+        static const AkUniqueID HUGEDOOR = 3947373296U;
+        static const AkUniqueID HURT = 3193947170U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LAND = 674522502U;
+        static const AkUniqueID MEDIUMDOOR = 4086418528U;
+        static const AkUniqueID MENUMUSIC = 679636833U;
+        static const AkUniqueID PLATEIDLE = 3561744037U;
+        static const AkUniqueID PLATEUNLOCK = 3179661459U;
+        static const AkUniqueID PORTALENTER = 193618431U;
+        static const AkUniqueID PORTALIDLE = 1625375393U;
+        static const AkUniqueID PORTALSWIRL = 3435179084U;
+        static const AkUniqueID QUICKTURN = 2747498379U;
+        static const AkUniqueID RESPAWN = 4279841335U;
         static const AkUniqueID RETURN = 3859834159U;
         static const AkUniqueID SELECT = 1432588725U;
+        static const AkUniqueID SHIELD = 1161967626U;
+        static const AkUniqueID SMALLDOOR = 97938166U;
+        static const AkUniqueID SWING = 2386519981U;
+        static const AkUniqueID WATER = 2654748154U;
+        static const AkUniqueID WEAPONIDLE = 3966510143U;
+        static const AkUniqueID WEAPONPICK = 4213176998U;
+        static const AkUniqueID WIND = 1537061107U;
     } // namespace EVENTS
 
     namespace SWITCHES
     {
+        namespace DEATHSURFACETYPE
+        {
+            static const AkUniqueID GROUP = 2510861594U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ACID = 3554728690U;
+                static const AkUniqueID GROUND = 2528658256U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace DEATHSURFACETYPE
+
         namespace SURFACETYPE
         {
             static const AkUniqueID GROUP = 63790334U;
 
             namespace SWITCH
             {
+                static const AkUniqueID EARTH = 3747705955U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID PUDDLE = 2003927601U;
+                static const AkUniqueID STONE = 1216965916U;
             } // namespace SWITCH
         } // namespace SURFACETYPE
 
     } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
